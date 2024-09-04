8efd6a1fe3c25456435e6b124a78b185d6756abd
dyf96 <yingfei.dai@atrae.co.jp>
2024-09-04 16:36:00 +0900

    refactor: 🔨 format lint

--- src/core/interfaces/IPerformanceService.ts
+++ src/core/interfaces/IPerformanceService.ts
@@ -2,11 +2,12 @@ export interface IPerformanceService {
   batchProcess<T>(
     items: T[],
     batchSize: number,
-    processFunction: (batch: T[]) => Promise<void>
-  ): Promise<void>;
+    processFunction: (batch: T[]) => Promise<void>,
+  ): Promise<void>
+
   parallelProcess<T>(
     items: T[],
     workerCount: number,
-    processFunction: (item: T) => Promise<void>
-  ): Promise<void>;
+    processFunction: (item: T) => Promise<void>,
+  ): Promise<void>
 }
